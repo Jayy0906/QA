6cbf6ac0e6fadc160dec94105835ead03555cad4
Jay patel <jayp962001@gmail.com>
2024-09-03 12:05:47 +0530

    Updated README.md

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
+    video: true, // Enable video recording
+    videoUploadOnPasses: false, // Disable video upload for passed tests (optional)
+    videosFolder: 'cypress/videos', // Folder where videos will be saved
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
